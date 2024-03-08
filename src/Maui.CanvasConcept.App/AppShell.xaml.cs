d25e75d601a17a29005d3bf546bce9ab63e7d250
Daniel Ancines <daniel.ancines@gmail.com>
2024-03-08 19:56:43 -0300

    First Commit

---
+++ src/Maui.CanvasConcept.App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Maui.CanvasConcept.App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
